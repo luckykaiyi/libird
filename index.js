f8e9da20ad02e182630f2708b78542d088f5f630
zhangkaiyi <zhangkaiyi@baidu.com>
2017-01-14 01:11:49 +0800

    support more router

--- index.js
+++ index.js
@@ -5,49 +5,83 @@ var utils = require('./lib/utils.js');
 var libird = {
     port: 8888,
     dirPath: './',
-    router: {},
+    router: {
+        list: {},
+        get: function(path, cb) {
+            libird.setRouter(path, cb);
+        }
+    },
     setPort: function(port) {
         this.port = port;
     },
     setDirPath: function(path) {
         this.dirPath = path;
     },
-    setRouter: function(reqPath, cb) {
-        this.router[reqPath] = cb;
+    setRouter: function(path, cb) {
+        var index = path.indexOf(':');
+        var withParam, param;
+        if(index == -1) {
+            withParam = false;
+            param = null;
+        } else {
+            withParam = true;
+            param = path.substring(++index);
+        }
+        this.router.list[path] = {
+            withParam: withParam,
+            param: param,
+            run:cb
+        };
+    },
+    runRouter: function(path, req, res) {
+        this.router.list[path].run(req, res);
     },
     server: net.createServer(function(sock) {
         sock.on('data', function(data) {
-            var reqData = utils.parseReqHeaders(data.toString());
-            var reqPath = reqData.path;
-            if(reqPath == '/') {
-                reqPath = '/index.html';
-            }
-            if(reqPath.indexOf('.html') != -1) {
-                reqData.Accept = 'text/html';
-            } else if(reqPath.indexOf('.js') != -1) {
-                reqData.Accept = 'application/javascript';
-            } else if(reqPath.indexOf('.css') != -1) {
-                reqData.Accept = 'text/css';
-            }
-            var resBody, resHeader, resData;
-            if(libird.router[reqPath]) {
-                resBody = libird.router[reqPath](reqData);
-                resHeader = 'HTTP/1.1 200 OK\nContent-Type: ' + reqData.Accept + '\nContent-Length: ' + Buffer.byteLength(resBody,'utf8') + '\n\n';
-                resData = resHeader + resBody;
-                sock.write(resData);
-            }else {
-                var filePath = libird.dirPath + reqPath;
+            var req = utils.parseReqHeaders(data.toString());
+            var res = {
+                header: {
+                    protocol: 'HTTP/1.1',
+                    status: '200 OK',
+                    contentType: utils.getContentType(req),
+                },
+                body:'',
+                getResData: function() {
+                    var resHeader = this.header.protocol + ' ' + this.header.status + '\n' + 'Content-Type:' + this.header.contentType + '\n' + 'Content-Length:' + Buffer.byteLength(this.body, 'utf-8') + '\n\n';
+                    var resBody = this.body;
+                    var resData = resHeader + resBody;
+                    return resData;
+                },
+                send:function(data) {
+                    this.body = JSON.stringify(data);
+                    var resData = this.getResData();
+                    sock.write(resData);
+                }
+            };
+            var path = req.path;
+            var routerPath = utils.matchRouter(path, libird.router.list);
+            if(routerPath) {
+                var item =libird.router.list[routerPath];
+                if (item.withParam) {
+                    var k = item.param;
+                    var v = path.match(/[^\/]+$/)[0];
+                    req.params[k] = v;
+                }
+                libird.runRouter(routerPath, req, res);
+            } else {
+                if (path == '/') {
+                    path = '/index.html'
+                }
+                var filePath = libird.dirPath + path;
                 fs.readFile(filePath, 'utf-8', function(err, data) {
                     if(err) {
                         console.log(err);
-                        resBody = '404 Not Found!'
-                        resHeader = 'HTTP/1.1 404 NotFound\nContent-Type: ' + reqData.Accept + '\nContent-Length: ' + Buffer.byteLength(resBody,'utf8') + '\n\n';
+                        res.header.status = '404 Not Found';
+                        res.body = '404 Not Found!';
                     }else {
-                        resBody = data;
-                        resHeader = 'HTTP/1.1 200 OK\nContent-Type: ' + reqData.Accept + '\nContent-Length: ' + Buffer.byteLength(resBody,'utf8') + '\n\n';
+                        res.body = data;
                     }
-                    resData = resHeader + resBody;
-                    //console.log('[[[',filePath,resData,']]]');
+                    var resData = res.getResData();
                     sock.write(resData);
                 })
             }
